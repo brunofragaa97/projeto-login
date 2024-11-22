43e67c96beab516e21608bf2f19dd6d157ddbd5f
Bruno de Fraga <37921531+brunofragaa97@users.noreply.github.com>
2024-11-22 10:32:23 -0300

    criado método async para solicitação de dados para o servidor, servidor ainda não configurado

---
+++ projeto-login/src/TelaLogin.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function TelaLogin() {
+  const [count, setCount] = useState(0)
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginClick = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('/api/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+      if (response.ok) {
+        console.log("login bem sucessido, parabens!!!Seja bem vindo ${data.user}");
+      } else {
+        console.log("Erro ao logar!!! Mensagem do servidor: ${data.message}")
+      }
+    } catch (error) {
+      console.log("Erro ao conectar no servidor")
+    }
+  }
+
+
+  return (
+    <>
+      <div className='main-container'>
+        <div className='animation-gif'>
+          <div className='container-login'>
+            <div className='container-login-caixa'>
+              <div><h1>LOGUE-SE</h1></div>
+              <form onSubmit={loginClick}>
+                <div>
+                  <label for="username">Usuario</label>
+                  <input 
+                  type='text' 
+                  value={username} 
+                  id="username" 
+                  name='username' 
+                  onChange={(e) => setUsername(e.target.value)}required></input>
+                </div>
+                <div>
+                  <label for="userpassword">Senha</label>
+                  <input
+                    type='password'
+                    value={password}
+                    id='userpassword'
+                    name='userpassword'
+                    onChange={(e) => setPassword(e.target.value)}required></input>
+                </div>
+                <div>
+                  <button type="submit" id='button-login'>ENTRAR</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default TelaLogin
