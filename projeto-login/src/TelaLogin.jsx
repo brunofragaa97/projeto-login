c4ca67413d4dc78efd6dd88a73f783171f8ca569
Bruno de Fraga <37921531+brunofragaa97@users.noreply.github.com>
2024-11-22 23:23:57 -0300

    nao sei oq foi feito neste commit

--- projeto-login/src/TelaLogin.jsx
+++ projeto-login/src/TelaLogin.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import './telaLogin.css'
 
 function TelaLogin() {
   const [count, setCount] = useState(0)
