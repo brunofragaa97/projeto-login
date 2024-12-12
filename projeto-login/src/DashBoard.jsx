d07151936f54644cac97ae102831332888100a34
Bruno de Fraga <37921531+brunofragaa97@users.noreply.github.com>
2024-12-12 13:38:46 -0300

    atualizacoes basicas de mensagens de erro, nenhuma arquivo realmente alterado ou modificado

--- projeto-login/src/DashBoard.jsx
+++ projeto-login/src/DashBoard.jsx
@@ -1,59 +1,60 @@
 import { useEffect, useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
-import './Styles/desktop/dashboard.css'
-import './Styles/mobile/dashboard.css'
+import "./Styles/desktop/dashboard.css";
+import "./Styles/mobile/dashboard.css";
 
 function DashBoard() {
   const navigate = useNavigate();
-  const [usuarioLogado, setUsuarioLogado] = useState(localStorage.getItem('usuarioLogado'))  
-  const [userName, setUserName] = useState("")
-  const token = localStorage.getItem("authToken")
-  const usuarioErrado = "bruno"
- 
+  const [usuarioLogado, setUsuarioLogado] = useState(
+    localStorage.getItem("usuarioLogado")
+  );
+  const [userName, setUserName] = useState("");
+  const token = localStorage.getItem("authToken");
+  const usuarioErrado = "bruno";
 
   useEffect(() => {
     const serverConection = async () => {
-        try {
-          const response = await fetch(`http://localhost:5000/api/validar-usuario?username=${usuarioLogado}`, {
-            method: 'GET'
-          });
-          
-          const servidor = await response.json();
-          if (!response.ok) {
-            console.log("Erro do servidor:", servidor.message);
-            console.log("CAIU NO IF");
-            navigate('/'); 
-          } else {
-            console.log(`${servidor.message}  /   ${usuarioLogado} / Seu token é  = ${token}`);
-            console.log("caiu no else, sucesso!");
+      try {
+        const response = await fetch(
+          `http://localhost:5000/api/validar-usuario?username=${usuarioLogado}`,
+          {
+            method: "GET",
+            
           }
+        );
 
-
-
-        } catch (error) {
-          console.log("Servidor indisponivel, Caiu no catch")
-          navigate('/')
+        const servidor = await response.json();
+        if (!response.ok) {
+          console.log("Erro do servidor:", servidor.message);
+          console.log("CAIU NO IF");
+          navigate("/");
+        } else {
+          console.log(
+            `${servidor.message}  /   ${usuarioLogado} / Seu token é  = ${token}`
+          );
+          console.log("caiu no else, sucesso!");
         }
+      } catch (error) {
+        console.log("Servidor indisponivel, Caiu no catch");
+        navigate("/");
+      }
     };
-    serverConection()
-  }, [])
-
-  
-
-
+    serverConection();
+  }, []);
 
   return (
     <div>
       <div className="main-container">
-        <div className="span1-container">Seja bem vindo, de volta Sr: {userName}</div>
+        <div className="span1-container">
+          Seja bem vindo, de volta Sr: {userName}
+        </div>
 
-        <div className="span2-container"><span>O que você deseja fazer hoje?</span>
+        <div className="span2-container">
+          <span>O que você deseja fazer hoje?</span>
         </div>
       </div>
-    </div >
-  )
+    </div>
+  );
 }
-
-
 
 export default DashBoard;
